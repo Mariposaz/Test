9fc65c6d2d3f6c44373b32ba0278aa0b3300c27c
Mariposaz <80291152+Mariposaz@users.noreply.github.com>
2021-03-09 10:28:50 -1000

    Update lion.sol

--- lion.sol
+++ lion.sol
@@ -1,9 +1,12 @@
 // contracts/bep-20.sol
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity 0.7.4;
 
-import "./ERC20.sol";
+import "@openzeppelin/contracts/GSN/Context.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TropicalFinance is ERC20 {
     constructor(uint256 initialSupply) ERC20("Tropical Finance", "TROPICAL") {
